ef274461724ba05e74f9814b84fea37f1baaab57
Majid <majidbabak@gmail.com>
2024-12-22 02:08:05 +0330

    fix(api): add async-await to functions and resolve stale variable reference

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 import axios from 'axios';
@@ -13,23 +13,23 @@ export default function App() {
     setBooks([...books, response.data]);
   };
 
-  const fetchBooks = () => {
-    axios.get('http://localhost:3001/books').then((res) => {
+  const fetchBooks = useCallback(async () => {
+    await axios.get('http://localhost:3001/books').then((res) => {
       setBooks(res.data);
     });
-  };
+  }, []);
 
   useEffect(() => {
     fetchBooks();
-  }, []);
+  }, [fetchBooks]);
 
-  const handleDeleteBook = (id) => {
-    axios.delete(`http://localhost:3001/books/${id}`);
+  const handleDeleteBook = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     setBooks(books.filter((book) => book.id !== id));
   };
 
-  function handleEditBook(id, newTitle) {
-    axios
+  async function handleEditBook(id, newTitle) {
+    await axios
       .put(`http://localhost:3001/books/${id}`, {
         title: newTitle
       })
